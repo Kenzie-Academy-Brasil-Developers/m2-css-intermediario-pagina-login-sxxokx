0257f319339ef1e87c5d75fae36cd5028b9e48d3
Jean <jean.angelo.borges@gmail.com>
2022-09-22 12:00:43 -0300

    atualizado

--- scripts/modal.js
+++ scripts/modal.js
@@ -13,64 +13,65 @@
 
     </section>
     */
-function createModal(){
-    const seclModal = document.createElement('section')
-    const divModal = documen.createElement('div')
-    const h3Modal = document.createElement('h3')
-    const closeModal = document.createElement('button')
-    const formModal = document.createElement('form')
-    const labelModal = document.createElement('label')
-    const inputModal = document.createElement('input')
-    const outroModal = document.createElement('button')
-    const recuperaModal = document.createElement('button')
-
-    //class
-    seclModal.classList.add('modal')
-    seclModal.id = 'modal_container'
-
-    divModal.classList.add('top-modal')
-
-    h3Modal.innerText = 'Recupere sua senha'
-
-    closeModal.classList.add('close-modal')
-    closeModal.id = 'close_modal'
-    closeModal.innerText = 'X'
-
-    formModal.classList.add('form-login')
-
-    labelModal.innerText = 'Digite o E-mail cadastrado na conta'
-
-    inputModal.placeholder = 'Digite o E-mail aqui...'
-
-    outroModal.classList.add('esqueci-senha')
-    outroModal.innerText = 'Usar outro meio de recuperação'
-
-    recuperaModal.classList.add('receber-email')
-    recuperaModal.innerText = 'Enviar para o E-mail'
-
-    divModal.append(h3Modal, closeModal)
-    formModal.append(labelModal, inputModal, outroModal, recuperaModal)
-    seclModal.append(divModal, formModal)
+    function createModal(){
+        const seclModal = document.createElement('section')
+        const divModal = document.createElement('div')
+        const h3Modal = document.createElement('h3')
+        const closeModal = document.createElement('button')
+        const formModal = document.createElement('form')
+        const labelModal = document.createElement('label')
+        const inputModal = document.createElement('input')
+        const outroModal = document.createElement('button')
+        const recuperaModal = document.createElement('button')
     
-    return seclModal
-}
-
-function showModal(){
-    const modalButton = document.getElementById('recuperar-senha')
-    const mainContainer = document.getElementById('main-container')
-
-    modalButton.addEventListener('click', () => {
-        const telaModal = createModal()
-        mainContainer.appendChild(telaModal)
-        closeModal()
-    })
-}
-showModal()
-
-function closeModal(){
-    const modalClose = document.getElementById('close_modal')
-    const seclModal = document.getElementById('modal_container')
-    modalClose.addEventListener('click', () =>{
-        seclModal.remove()
-    })
-}
+        //class
+        seclModal.classList = 'modal bg-black '
+        seclModal.id = 'modal_container'
+    
+        divModal.classList.add('top-modal')
+    
+        h3Modal.innerText = 'Recupere sua senha'
+    
+        closeModal.classList.add('close-modal')
+        closeModal.id = 'close_modal'
+        closeModal.innerText = 'X'
+    
+        formModal.classList.add('form-login')
+    
+        labelModal.innerText = 'Digite o E-mail cadastrado na conta'
+    
+        inputModal.placeholder = 'Digite o E-mail aqui...'
+    
+    
+    
+        recuperaModal.classList.add('receber-email')
+        recuperaModal.innerText = 'Enviar para o E-mail'
+    
+        divModal.append(h3Modal, closeModal)
+        formModal.append(labelModal, inputModal, outroModal, recuperaModal)
+        seclModal.append(divModal, formModal)
+        
+        return seclModal
+    }
+    
+    function showModal(){
+        const modalButton = document.getElementById('esqueceu')
+        const mainContainer = document.getElementById('main-container')
+    
+        modalButton.addEventListener('click', (event) => {
+            event.preventDefault()
+            console.log("click")
+            const telaModal = createModal()
+            mainContainer.appendChild(telaModal)
+            closeModal()
+        })
+    }
+    
+    function closeModal(){
+        const modalClose = document.getElementById('close_modal')
+        const seclModal = document.getElementById('modal_container')
+        modalClose.addEventListener('click', () =>{
+            seclModal.remove()
+        })
+    }
+    showModal()
